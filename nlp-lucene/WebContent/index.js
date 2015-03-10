0ac4f9e7b948729b31d6f3802dcccb03b3032e06
kallurivenkatesh <VKCD9@mail.umkc.edu>
2015-03-10 15:40:44 -0500

    changing by sfc

---
+++ nlp-lucene/WebContent/index.js
@@ -0,0 +1,56 @@
+var base_url = window.location.origin;
+
+function performAllActions() {
+	clearResult();
+	tokenize();
+	ngrams();
+}
+
+function tokenize() {
+	var sentence = $("#sentence");
+	var url = base_url + "/api/service/tokenize/"+ encodeURI(sentence.val());
+	perform(url);
+}
+
+function ngrams() {
+	var sentence = $("#sentence");
+	var url = base_url + "/api/service/ngram/"+ encodeURI(sentence.val());
+	perform(url);
+}
+
+function synonymns() {
+	var sentence = $("#sentence");
+	var url = base_url + "/api/service/synon/"+ encodeURI(sentence.val());
+	perform(url);
+}
+
+function parents() {
+	var sentence = $("#sentence");
+	var str = sentence.val();
+	var res = str.split(",");
+	if(res.length > 2) {
+		alert("Two words please, separated by a , ");
+		return;
+	}
+	
+	var url = base_url + "/api/service/parents/"+ encodeURI(res[0])+"/"+encodeURI(res[1]);
+	perform(url);
+}
+
+function perform(url) {
+	console.log(url);
+	var resultDiv = $("#result");
+	$.get(url, function(data) {
+		console.log(data);
+//		resultDiv.text(JSON.stringify(data));
+		resultDiv.append(JSON.stringify(data));
+	}).fail(function(data) {
+		console.log(data);
+		alert(data);
+	});
+}
+
+function clearResult() {
+	var resultDiv = $("#result");
+	resultDiv.text("");
+}
